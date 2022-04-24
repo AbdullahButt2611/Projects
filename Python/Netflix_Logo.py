3b15349f74cf83aa0aa5a3b7fece13082d0bbc24
M. Abdullah Butt <81871162+AbdullahButt2611@users.noreply.github.com>
2022-04-24 23:38:27 +0500

    Netflix Logo
    This is a code to make the netflix logo in python. using turtle library. It is a small try toward the graphics of python.

---
+++ Python/Netflix_Logo.py
@@ -0,0 +1,34 @@
+from turtle import *
+
+
+pos = [(-40, 0), (40, 0)]
+right(90)
+
+bgcolor('black')
+for x, y in pos:
+    up()
+    goto(x, y)
+    down()
+
+
+    fillcolor('red')
+    begin_fill()
+    for i in range(2):
+        forward(200)
+        left(90)
+        forward(40)
+        left(90)
+    end_fill()
+
+up()
+goto(-40, 0)
+down()
+left(22)
+begin_fill()
+for i in range(2):
+    forward(220)
+    left(68)
+    forward(40)
+    left(112)
+end_fill()
+input()
