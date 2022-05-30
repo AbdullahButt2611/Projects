7803411e4e3801726bf1b70df70210c688fc369d
M. Abdullah Butt <81871162+AbdullahButt2611@users.noreply.github.com>
2022-05-30 19:32:21 +0500

    Shopping Mall
    A C# based Modern GUI model

---
+++ C#/Modern Flat UI/Shopping Mall/Shopping Mall/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FontAwesome.Sharp;
+
+namespace Shopping_Mall
+{
+    public partial class Form1 : Form
+    {
+        //fields
+        private IconButton currentBtn;
+        private Panel leftBorderBtn;
+
+        //Structs
+        private struct RGBColor{
+            public static Color color1 = Color.FromArgb(172,126,241);
+            public static Color color2 = Color.FromArgb(249,118,176);
+            public static Color color3 = Color.FromArgb(253,138,114);
+            public static Color color4 = Color.FromArgb(95,77,221);
+            public static Color color5 = Color.FromArgb(249,88,155);
+            public static Color color6 = Color.FromArgb(24,161,251);
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            leftBorderBtn = new Panel();
+            leftBorderBtn.Size = new Size(7,60);
+            panelMenu.Controls.Add(leftBorderBtn);
+        }
+
+
+        //Methods
+        private void ActivateButton(object SenderBtn, Color color)
+        {
+            if(SenderBtn!=null)
+            {
+                DisableBtn();
+                currentBtn = (IconButton)SenderBtn;
+                currentBtn.BackColor = Color.FromArgb(37, 36, 81);
+                currentBtn.ForeColor = color;
+                currentBtn.TextAlign = ContentAlignment.MiddleCenter;
+                currentBtn.IconColor = color;
+                currentBtn.TextImageRelation = TextImageRelation.TextBeforeImage;
+                currentBtn.ImageAlign = ContentAlignment.MiddleRight;
+                leftBorderBtn.BackColor = color;
+                leftBorderBtn.Location = new Point(0,currentBtn.Location.Y);
+                leftBorderBtn.Visible = true;
+                leftBorderBtn.BringToFront();
+            }
+        }
+
+        private void DisableBtn()
+        {
+            if(currentBtn!= null)
+            {
+                currentBtn.BackColor = Color.FromArgb(31,30,68);
+                currentBtn.ForeColor = Color.Gainsboro;
+                currentBtn.TextAlign = ContentAlignment.MiddleLeft;
+                currentBtn.IconColor = Color.Gainsboro;
+                currentBtn.TextImageRelation = TextImageRelation.ImageBeforeText;
+                currentBtn.ImageAlign = ContentAlignment.MiddleLeft;
+            }
+        }
+
+        private void btnDashboard_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender,RGBColor.color1);
+        }
+
+        private void btnOrders_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColor.color2);
+        }
+
+        private void btnProducts_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColor.color3);
+        }
+
+        private void btnCustomers_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColor.color4);
+        }
+
+        private void btnMarket_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColor.color5);
+        }
+
+        private void btnSettings_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColor.color6);
+        }
+
+        //12:45
+    }
+}
