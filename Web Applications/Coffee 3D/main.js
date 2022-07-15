69822153011776de8a2a2aab5308fe11d93b2e25
M. Abdullah Butt <81871162+AbdullahButt2611@users.noreply.github.com>
2022-07-15 20:18:12 +0500

    Menu
    The menu of the application's landing page is ready and can be toggled.

---
+++ Web Applications/Coffee 3D/main.js
@@ -0,0 +1,29 @@
+// ==================   Menu Show Y Hidden  ============================
+const navMenu = document.getElementById("nav-menu");
+const toggleMenu = document.getElementById("nav-toggle");
+const closeMenu = document.getElementById("nav-close");
+
+
+// ========   Show  =========
+toggleMenu.addEventListener("click", ()=>{
+    navMenu.classList.toggle('show');
+});
+
+// ========   Hidden  ==========
+closeMenu.addEventListener("click", ()=>{
+    navMenu.classList.remove('show');
+});
+
+
+// ==================   Mouse Move Home Img  ===============================
+// ==================   GSAP Animation  ===============================
+// Nav
+// Menu
+
+
+
+
+
+// ==================   Menu Show Y Hidden  ===============================
+// ==================   Menu Show Y Hidden  ===============================
+// ==================   Menu Show Y Hidden  ===============================
