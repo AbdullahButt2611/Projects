53871aa9a2c0d3bc500b452cab41bb43a19c3acc
M. Abdullah Butt <81871162+AbdullahButt2611@users.noreply.github.com>
2022-08-24 12:50:54 +0500

    Head Section Completed

--- Python/Doraemon.py
+++ Python/Doraemon.py
@@ -32,3 +32,115 @@ def drawRect(length, width, filled):
     forward(width)
     if filled == True:
         end_fill()
+
+def head():
+    color("blue", "blue")
+    penup()
+    goto(0, 100)
+    drawRound(75, True)
+
+    color("white", "white")
+    penup()
+    goto(0, 72)
+    drawRound(60, True)
+
+
+def eyes():
+    # Left Eye Socket
+    color("black", "white")
+    penup()
+    goto(-15, 80)
+    drawRound(17, True)
+
+    #Right Eye Socket
+    color("black", "white")
+    penup()
+    goto(19, 80)
+    drawRound(17, True)
+
+    color("black", "black")
+    penup()
+    goto(-8, 70)
+    drawRound(6, True)
+
+    color("white", "white")
+    penup()
+    goto(-8, 66)
+    drawRound(2, True)
+
+    #Right Eye Ball
+    color("black", "black")
+    penup()
+    goto(12, 70)
+    drawRound(6, True)
+
+    color("white", "white")
+    penup()
+    goto(12, 66)
+    drawRound(2, True)
+
+
+def nose():
+    color("red", "red")
+    penup()
+    goto(0, 40)
+    drawRound(7, True)
+
+
+def mouth():
+    #mouth
+    color("black", "black")
+    penup()
+    goto(-30, -20)
+    pendown()
+    setheading(-27)
+    circle(70, 55)
+
+    penup()
+    goto(0, 26)
+    pendown()
+    goto(0, -25)
+
+
+def whiskers():
+    color("black", "black")
+    # The beard in the middle on the left
+    penup()
+    goto(10, 5)
+    pendown()
+    goto(-40, 5)
+    # The beard in the middle on the right
+    penup()
+    goto(10, 5)
+    pendown()
+    goto(40, 5)
+
+    # The beard in the upper on the left
+    penup()
+    goto(10, 15)
+    pendown()
+    goto(-40, 20)
+    # The beard in the upper on the right
+    penup()
+    goto(10, 15)
+    pendown()
+    goto(40, 20)
+
+    #left beard
+    penup()
+    goto(-10, -5)
+    pendown()
+    goto(-40, -10)
+
+    #Beard on lower right
+    penup()
+    goto(10, -5)
+    pendown()
+    goto(40, -10)
+
+
+head()
+eyes()
+nose()
+mouth()
+whiskers()
